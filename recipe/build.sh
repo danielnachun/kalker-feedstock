e8489cf75ee1662b7b118a3ed19f1c7955f3ae63
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-16 13:52:41 -0700

    enable platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@ echo '[features]' >> cli/Cargo.toml
 echo 'use-system-gmp = "gmp-mpfr-sys/use-system-libs"' >> cli/Cargo.toml
 
 # build statically linked binary with Rust
-cargo install --bins --no-track --locked --root ${PREFIX} --path cli
+cargo install --bins --no-track --locked --root ${PREFIX} --path cli --features use-system-gmp
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/${PKG_NAME}"
